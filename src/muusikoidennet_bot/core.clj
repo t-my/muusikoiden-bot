e080c4f5f0e4c9e00492db8cc748e008196dff41
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@metosin.fi>
2026-05-05 11:25:10 +0300

    Add user-agent

--- src/muusikoidennet_bot/core.clj
+++ src/muusikoidennet_bot/core.clj
@@ -440,7 +440,7 @@
                         :indent true)))
 
 (defn fetch-page [url]
-  (-> (http/get url {:headers {"User-Agent" "Mozilla/5.0 (compatible; MuusikoidenBot/1.0)"}})
+  (-> (http/get url {:headers {"User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}})
       :body
       (html/html-snippet)))
 
